69e082ef1e1b2e998cc0cdcdb20efc9758bd80ed
Bryan Guevara <bryanguevara@Bryans-MacBook-Air.local>
2021-12-29 20:53:09 -0800

    Grid is now visible in screen. Means for resizing have been added but implementation still pending

--- main.js
+++ main.js
@@ -1,4 +1,83 @@
-const h1Tag = document.createElement("h1");
-h1Tag.textContent = "WE ARE ETCHING AND SKETCHING";
-h1Tag.style.color = "red";
-document.body.appendChild(h1Tag);
+class SquareBoard {
+    grid =  [];
+    dimention = 0;
+    cell_width = 0;
+    cell_height = 0;
+    color = "rgb(255, 255, 255)";
+    html_board;
+    constructor(dim, html_e){
+        this.html_board = html_e;
+        this.dimention = dim;
+        this.cell_width = this.html_board.clientWidth / this.dimention;
+        this.cell_height = this.html_board.clientHeight / this.dimention;
+
+
+        for(let row = 0; row < this.dimention; row++){
+            let cells = [];
+            for(let cell = 0; cell < this.dimention; cell++){
+                // add the cell class
+                const div_cell = document.createElement("div");
+                div_cell.classList.add("cell");
+
+                // add event listener to change background color
+                div_cell.addEventListener("mouseenter", () => {
+                    div_cell.style.backgroundColor = this.color;
+                });
+
+                // set cell width and height;
+                div_cell.style.width = `${this.cell_width}px`;
+                div_cell.style.height = `${this.cell_height}px`;
+
+                // add to the board and display.
+                cells.push(div_cell);
+                this.html_board.appendChild(div_cell);
+            }
+
+            this.grid.push(cells);
+        }
+    }
+
+    resize(dim){
+        this.grid = [];
+        this.dimention = dim;
+        this.cell_width = this.html_board.clientWidth / this.dimention;
+        this.cell_height = this.html_board.clientHeight / this.dimention;
+        this.html_board.innerHTML = "";
+
+        for(let row = 0; row < this.dimention; row++){
+            let cells = [];
+            for(let cell = 0; cell < this.dimention; cell++){
+                // add the cell class
+                const div_cell = document.createElement("div");
+                div_cell.classList.add("cell");
+
+                // add event listener to change background color
+                div_cell.addEventListener("mouseenter", () => {
+                    div_cell.style.backgroundColor = this.color;
+                });
+
+                // set cell width and height;
+                div_cell.style.width = `${this.cell_width}px`;
+                div_cell.style.height = `${this.cell_height}px`;
+
+                // add to the board and display.
+                cells.push(div_cell);
+                this.html_board.appendChild(div_cell);
+            }
+
+            this.grid.push(cells);
+        }
+    }
+};
+
+let grid = document.querySelector(".container > #grid");
+
+// Initial board to be displayed
+let current_board = new SquareBoard(16, grid);
+
+
+
+// Resizes the grid when the screen changes size;
+window.addEventListener("resize", ()=>{
+    current_board.resize(current_board.dimention, grid);
+});
